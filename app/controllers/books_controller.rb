164b24d38cb61c839f6bcd162f13eeb7614951bc
ayacatt <k.saiko0224@gmail.com>
2021-10-01 09:11:14 +0000

    Initial Commit2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,19 @@
 class BooksController < ApplicationController
+  def new
+    @book = Book.new
+    @user = current_user.posts.build
+  end
 
   def show
     @book = Book.find(params[:id])
+    @user = @book.user
+    @users = User.all
+
   end
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def create
@@ -21,29 +29,32 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
+    if @book.user == current_user
+      render :edit
+    else
+      redirect_to books_path
+    end
   end
-
-
 
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
-      redirect_to book_path(@book), notice: "You have updated book successfully."
+      redirect_to book_path(@book.id), notice: "You have updated book successfully."
     else
-      render "edit"
+      render :edit
     end
   end
 
-  def delete
-    @book = Book.find(params[:id])
-    @book.destoy
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
     redirect_to books_path
   end
 
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title, :body)
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,13 +3,14 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @books = @user.books
+    @books = Book.where(user_id: @user.id)
     @book = Book.new
   end
 
   def index
     @users = User.all
     @book = Book.new
+  end
 
   def edit
     @user = User.find(params[:id])
@@ -17,9 +18,9 @@ class UsersController < ApplicationController
 
   def update
     if @user.update(user_params)
-      redirect_to users_path(@user), notice: "You have updated user successfully."
+      redirect_to user_path(@user.id), notice: "You have updated user successfully."
     else
-      render "show"
+      render :edit
     end
   end
 
@@ -34,3 +35,4 @@ class UsersController < ApplicationController
       redirect_to user_path(current_user)
     end
   end
+end
