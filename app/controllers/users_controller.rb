f6a4556f6dd2b70056ce67d2a7bfcc72d214b0fb
ayacatt <k.saiko0224@gmail.com>
2021-10-02 06:56:56 +0000

    Finish

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-  before_action :ensure_correct_user, only: [:update]
-
+  before_action :ensure_correct_user, only: [:edit, :update]
+  
   def show
     @user = User.find(params[:id])
     @books = Book.where(user_id: @user.id)
@@ -14,6 +14,9 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
+    if @user != current_user
+      redirect_to user_path(current_user)
+    end
   end
 
   def update
