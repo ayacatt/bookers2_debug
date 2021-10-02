05d591f318525bf774d5d350a447e1def66b0551
ayacatt <k.saiko0224@gmail.com>
2021-10-02 07:14:40 +0000

    Finish

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,5 @@
 class BooksController < ApplicationController
+  before_action :ensure_correct_user, only: [:edit, :update, :destroy]
   def new
     @book = Book.new
     @user = current_user.posts.build
@@ -57,5 +58,10 @@ class BooksController < ApplicationController
   def book_params
     params.require(:book).permit(:title, :body)
   end
-
+  def ensure_correct_user
+    @book = Book.find(params[:id])
+    unless @book.user == current_user
+    redirect_to books_path
+    end
+  end
 end
